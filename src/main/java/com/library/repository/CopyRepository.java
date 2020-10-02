e4f44b5b86e72928f54193108602d24686b93781
robert-er <robert.roza.pl@gmail.com>
2020-10-02 12:11:43 +0200

    create repositories

---
+++ src/main/java/com/library/repository/CopyRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+import com.library.model.Copy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CopyRepository extends JpaRepository<Copy, Long> {
+}
---
+++ src/main/java/com/library/repository/RentalRepository.java
@@ -0,0 +1,7 @@
+package com.library.repository;
+
+import com.library.model.Rental;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RentalRepository extends JpaRepository<Rental, Long>  {
+}
